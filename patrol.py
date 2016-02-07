26842709474adf77df585c9d6d10c1ea5f371dc9
Andrew Enfield <aenfield@gmail.com>
2016-02-07 10:34:50 -0800

    Added assignment field, set randomly at patrol creation.

--- patrol.py
+++ patrol.py
@@ -1,4 +1,4 @@
-#import collections
+import dice
 
 class Patrol(object):
     patrol_assignments = {'Pearl Harbor - 1941':
@@ -22,6 +22,10 @@ class Patrol(object):
                           'Australia - 1944 Jul - Dec':0,
                           'Australia - 1945 Jan - Jun':0,
                          }
+
+    def __init__(self, port_and_date, random_seed=None):
+        self.port_and_date = port_and_date
+        self.assignment = self.patrol_assignments[self.port_and_date][dice.Dice('2d6', random_seed).roll()]
 
     def get_ports_and_dates(self):
         return self.patrol_assignments.keys()
--- patrol_test.py
+++ patrol_test.py
@@ -5,15 +5,25 @@ import unittest
 import patrol
 
 class PatrolAssignment(unittest.TestCase):
+    a_port_and_date = 'Pearl Harbor - 1941'
 
     def test_can_get_list_of_ports_and_dates(self):
-        p = patrol.Patrol()
+        p = patrol.Patrol(self.a_port_and_date)
+        d = p.get_ports_and_dates()
 
-        sut = p.get_ports_and_dates()
+        self.assertEqual(len(d), 16)
+        self.assertIn('Pearl Harbor - 1941', d)
+        self.assertIn('Australia - 1945 Jan - Jun', d)
 
-        self.assertEqual(len(sut), 16)
-        self.assertIn("Pearl Harbor - 1941", sut)
-        self.assertIn("Australia - 1945 Jan - Jun", sut)
+    def test_can_get_assignment(self):
+        p = patrol.Patrol('Pearl Harbor - 1941')
+
+        self.assertIn(p.assignment, ['Empire','Marshalls'])
+
+    def test_can_get_assignment_with_seed(self):
+        p = patrol.Patrol('Pearl Harbor - 1941', random_seed=0)
+
+        self.assertEqual('Marshalls', p.assignment)
 
 
 
